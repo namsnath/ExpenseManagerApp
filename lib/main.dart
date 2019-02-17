98ab3b0a83c84b6951914787760c138c3f7260fd
Namit Nathwani <namsnath@gmail.com>
2019-02-17 14:52:47 +0530

    Added new routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'login.dart';
+import 'category.dart';
+import 'transactionList.dart';
+import 'home.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,10 +11,12 @@ class MyApp extends StatelessWidget {
 	@override
 	Widget build(BuildContext context) {
 		return MaterialApp(
-			initialRoute: 'loginPage',
+			initialRoute: '/',
 			routes: {
-				'homePage': (context) => HomePage(title: 'Expense Manager Home Page',),
-				'loginPage': (context) => LoginPage(title: 'Expense Manager Login Page'),
+				'categoryPage': (context) => CategoryPage(title: 'Expense Manager Category Page',),
+				'/': (context) => LoginPage(title: 'Expense Manager Login Page'),
+				'transactionListPage': (context) => TransactionListPage(title: 'Expense Manager Transaction List Page'),
+				'homePage': (context) => HomePage(title: 'Expense Manager Home Page'),
 			},
 			title: 'Expense Manager',
 			theme: ThemeData(
@@ -22,72 +26,3 @@ class MyApp extends StatelessWidget {
 	}
 }
 
-class HomePage extends StatefulWidget {
-	HomePage({Key key, this.title}) : super(key: key);
-	final String title;
-
-	@override
-	_HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-	@override
-	Widget build(BuildContext context) {
-		return Scaffold(
-			appBar: AppBar(title: Text('Categories')),
-			body: _buildBody(context),
-		);
-	}
-
-	Widget _buildBody(BuildContext context) {
-		return StreamBuilder<QuerySnapshot>(
-			stream: Firestore.instance.collection('categories').snapshots(),
-			builder: (context, snapshot) {
-				if (!snapshot.hasData) return LinearProgressIndicator();
-
-				return _buildList(context, snapshot.data.documents);
-			},
-		);
-	}
-
-	Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
-		return ListView(
-			padding: const EdgeInsets.only(top: 20.0),
-			children: snapshot.map((data) => _buildListItem(context, data)).toList(),
-		);
-	}
-
-	Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
-		final record = Record.fromSnapshot(data);
-
-		return Padding(
-			key: ValueKey(record.name),
-			padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-			child: Container(
-				decoration: BoxDecoration(
-					border: Border.all(color: Colors.grey),
-					borderRadius: BorderRadius.circular(5.0),
-				),
-				child: ListTile(
-					title: Text(record.name),
-					onTap: () => print(record),
-				),
-			),
-		);
-	}
-}
-
-class Record {
-	final String name;
-	final DocumentReference reference;
-
-	Record.fromMap(Map<String, dynamic> map, {this.reference})
-			: assert(map['name'] != null),
-				name = map['name'];
-
-	Record.fromSnapshot(DocumentSnapshot snapshot)
-			: this.fromMap(snapshot.data, reference: snapshot.reference);
-
-	@override
-	String toString() => "Record<$reference:$name>";
-}
